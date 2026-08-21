fbaa77442e789a93831b3150d1e6e6351edb8ca0
aetos <aetos382@users.noreply.github.com>
2026-08-21 20:30:29 +0900

    wip

--- Generators/TraceEventGenerator/IndentedStringBuilder.cs
+++ Generators/TraceEventGenerator/IndentedStringBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 
 namespace Aetos.Tracing;
@@ -5,4 +6,9 @@ namespace Aetos.Tracing;
 internal sealed class IndentedStringBuilder
 {
     private readonly StringBuilder _stringBuilder;
+
+    private readonly class IndentationScope : IDisposable
+    {
+
+    }
 }
